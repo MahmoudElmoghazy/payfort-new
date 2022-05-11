56f73a0be2debad2d7d357fe06d5e518c025dedb
Mahmoud El-moghazy <mahmod_masrawi@yahoo.com>
2022-05-11 21:06:27 +0200

    Handle android payment

--- android/src/main/java/com/reactnativepayfort/PayfortModule.java
+++ android/src/main/java/com/reactnativepayfort/PayfortModule.java
@@ -1,20 +1,57 @@
 package com.reactnativepayfort;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.ReactApplicationContext;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.bridge.Callback;
+
+import com.payfort.fortpaymentsdk.FortSdk;
+import com.payfort.fortpaymentsdk.callbacks.FortCallBackManager;
+import com.payfort.fortpaymentsdk.callbacks.FortCallback;
+import com.payfort.fortpaymentsdk.callbacks.FortInterfaces;
+import com.payfort.fortpaymentsdk.domain.model.FortRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 @ReactModule(name = PayfortModule.NAME)
-public class PayfortModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "Payfort";
+public class PayfortModule extends ReactContextBaseJavaModule
+  implements ActivityEventListener {
 
-    public PayfortModule(ReactApplicationContext reactContext) {
+  public static final String NAME = "Payfort";
+
+  private FortCallBackManager fortCallback = FortCallback.Factory.create();
+  private static ReactApplicationContext reactContext;
+
+  private Activity context = null;
+
+  public PayfortModule(ReactApplicationContext reactContext) {
         super(reactContext);
-    }
+
+        this.reactContext = reactContext;
+        this.reactContext.addActivityEventListener(this);
+
+        this.context = getCurrentActivity();
+   }
+
 
     @Override
     @NonNull
@@ -23,12 +60,80 @@ public class PayfortModule extends ReactContextBaseJavaModule {
     }
 
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
-    public void multiply(int a, int b, Promise promise) {
-        promise.resolve(a * b);
+    public void getDeviceId(Callback successCallback){
+        successCallback.invoke(FortSdk.getDeviceId(getCurrentActivity()));
     }
 
-    public static native int nativeMultiply(int a, int b);
+    @ReactMethod
+    public void Pay(String parameters, Callback successCallback, Callback errorCallback) {
+      try {
+        Map<String, Object> map = new TreeMap<String, Object>();
+        JSONObject jsonObject = new JSONObject(parameters);
+
+        String ENV = jsonObject.getBoolean("isLive") ? FortSdk.ENVIRONMENT.PRODUCTION : FortSdk.ENVIRONMENT.TEST;
+
+        for (Iterator<String> it = jsonObject.keys(); it.hasNext(); ) {
+          String key = it.next();
+
+          if(!key.equals("isLive") && !key.equals("token_name")) {
+            map.put(key, jsonObject.get(key).toString());
+          }
+        }
+
+        // .. create fucking request
+        FortRequest fortrequest = new FortRequest();
+        fortrequest.setRequestMap(map);
+
+        context = getCurrentActivity();
+
+        FortSdk.getInstance().registerCallback(context, fortrequest, ENV, 5, fortCallback, true, new FortInterfaces.OnTnxProcessed() {
+          @Override
+          public void onCancel(Map<String, Object> requestMap, Map<String, Object> responseMap) {
+            // Toast.makeText(reactContext, "Payment cancel by user", Toast.LENGTH_SHORT).show();
+            Log.d("Hello", "onCancel() called with: map = [" + requestMap + "], map1 = [" + responseMap + "]");
+            errorCallback.invoke(converMapToJson(responseMap));
+          }
+
+          @Override
+          public void onSuccess(Map<String, Object> requestMap, Map<String, Object> responseMap) {
+            //Toast.makeText(reactContext, "Payment Success", Toast.LENGTH_SHORT).show();
+            Log.d("Hello", "onSuccess() called with: map = [" + requestMap + "], map1 = [" + responseMap + "]");
+            successCallback.invoke(converMapToJson(responseMap));
+
+          }
+
+          @Override
+          public void onFailure(Map<String, Object> requestMap, Map<String, Object> responseMap) {
+            //Toast.makeText(reactContext, "Payment fail", Toast.LENGTH_SHORT).show();
+            Log.d("Hello", "onFailure() called with: map = [" + requestMap + "], map1 = [" + responseMap + "]");
+            errorCallback.invoke(converMapToJson(responseMap));
+          }
+        });
+      } catch (Exception e) {
+        Log.d("Exception", String.valueOf(e));
+//        errorCallback.invoke(e);
+      }
+
+    }
+
+  private String converMapToJson(Map<String, Object> source){
+    Gson gson=new Gson();
+    Type gsonType = new TypeToken<HashMap>(){}.getType();
+    String gsonString = gson.toJson(source,gsonType);
+    return gsonString;
+  }
+
+  @Override
+  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+//    super.onActivityResult(requestCode, resultCode, data);
+    fortCallback.onActivityResult(requestCode, resultCode, data);
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+
+  }
+
+
 }
