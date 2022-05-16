297e581bc506cfa5865faf32ce11ae7fc3954640
Mahmoud El-moghazy <mahmod_masrawi@yahoo.com>
2022-05-16 10:57:43 +0200

    Fix Name

--- ios/Payfort.m
+++ ios/Payfort.m
@@ -1,20 +1,8 @@
 #import "Payfort.h"
 
-@implementation Payfort
+@implementation PayFort
 
 RCT_EXPORT_MODULE()
-
-// Example method
-// See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply, multiplyWithA
-                 : (nonnull NSNumber *)a withB
-                 : (nonnull NSNumber *)b withResolver
-                 : (RCTPromiseResolveBlock)resolve withRejecter
-                 : (RCTPromiseRejectBlock)reject) {
-  NSNumber *result = @([a floatValue] * [b floatValue]);
-
-  resolve(result);
-}
 
 RCT_EXPORT_METHOD(Pay
                   : (NSString *)strData successCallback
